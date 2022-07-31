720e6bd032e1cc5747748f1f4064f0d5357a0827
victorcappa <cappacurta@gmail.com>
2022-07-31 18:37:25 -0300

    animations

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
 	const start = async () => {
 		const mindarThree = new window.MINDAR.IMAGE.MindARThree({
 			container: document.querySelector("#ar-container"),
-			imageTargetSrc: "./assets//targets/uno_logo.mind",
-			imageTargetSrc: "./assets//targets/uno_nulo.mind",
+			imageTargetSrc: "./assets//targets/uno.mind",
+			// imageTargetSrc: "./assets//targets/uno_nulo.mind",
 			maxTrack: 2,
 		});
 
@@ -14,6 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 		scene.add(light);
+
+		//models
 
 		const bear = await loadGLTF("./assets/models/musicband-bear/scene.gltf");
 		bear.scene.scale.set(0.1, 0.1, 0.1);
@@ -25,11 +27,21 @@ document.addEventListener("DOMContentLoaded", () => {
 		const raccoonAnchor = mindarThree.addAnchor(1);
 		raccoonAnchor.group.add(raccoon.scene);
 
+		// gltf animations
+		const mixer = new THREE.AnimationMixer(raccoon.scene);
+		const action = mixer.clipAction(raccoon.animations[0]);
+		action.play();
+
+		const clock = new THREE.Clock();
+
 		await mindarThree.start();
 
 		renderer.setAnimationLoop(() => {
+			const delta = clock.getDelta();
+			mixer.update(delta);
+			raccoon.scene.rotation.set(0, raccoon.scene.rotation.y + delta, 0);
 			renderer.render(scene, camera);
 		});
-	};
+	};;
 	start();
 });
