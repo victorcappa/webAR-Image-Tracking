d3b69e6ed6b3c8fcd25bd01a6b57952fde91915b
victorcappa <cappacurta@gmail.com>
2022-07-31 14:22:45 -0300

    tracking multiple images at the same time

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
-import { GLTFLoader } from "../libs/three.js-r132/examples/jsm/loaders/GLTFLoader.js";
 import { loadGLTF } from "../libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
-
-// const loadGLTF = (path) => {
-// 	return new Promise((resolve, reject) => {
-// 		const loader = new GLTFLoader();
-// 		loader.load(path, (gltf) => {
-// 			resolve(gltf);
-// 		});
-// 	});
-// };
 
 document.addEventListener("DOMContentLoaded", () => {
 	const start = async () => {
 		const mindarThree = new window.MINDAR.IMAGE.MindARThree({
 			container: document.querySelector("#ar-container"),
-			imageTargetSrc: "./assets/sucrilhos.mind",
+			imageTargetSrc: "./assets//targets/uno_logo.mind",
+			imageTargetSrc: "./assets//targets/uno_nulo.mind",
+			maxTrack: 2,
 		});
 
 		const { renderer, scene, camera } = mindarThree;
 
 		const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 		scene.add(light);
-		const anchor = mindarThree.addAnchor(0);
-		const gltf = await loadGLTF("./assets/models/musicband-bear/scene.gltf");
-		gltf.scene.scale.set(0.1, 0.1, 0.1);
-		gltf.scene.rotation.x = Math.PI / 2;
-		anchor.group.add(gltf.scene);
 
-		const loader = new GLTFLoader();
+		const bear = await loadGLTF("./assets/models/musicband-bear/scene.gltf");
+		bear.scene.scale.set(0.1, 0.1, 0.1);
+		const bearAnchor = mindarThree.addAnchor(0);
+		bearAnchor.group.add(bear.scene);
+
+		const raccoon = await loadGLTF("./assets/models/musicband-raccoon/scene.gltf");
+		raccoon.scene.scale.set(0.1, 0.1, 0.1);
+		const raccoonAnchor = mindarThree.addAnchor(1);
+		raccoonAnchor.group.add(raccoon.scene);
 
 		await mindarThree.start();
 
