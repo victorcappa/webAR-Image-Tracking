606e46e34dcbf1f6844aca45869d0bdecb3ab5c6
victorcappa <cappacurta@gmail.com>
2022-07-31 20:10:24 -0300

    sounds and animations

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-import { loadGLTF } from "../libs/loader.js";
+import { loadGLTF, loadAudio } from "../libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener("DOMContentLoaded", () => {
 	const start = async () => {
+		// change background color of body to none
+		document.body.style.backgroundColor = "transparent";
 		const mindarThree = new window.MINDAR.IMAGE.MindARThree({
 			container: document.querySelector("#ar-container"),
 			imageTargetSrc: "./assets//targets/uno.mind",
@@ -15,17 +17,31 @@ document.addEventListener("DOMContentLoaded", () => {
 		const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 		scene.add(light);
 
+		// audio
+
+		const audioclip = await loadAudio("./assets/audio/queRabao.mp3");
+		const listener = new THREE.AudioListener();
+		const audioSource = new THREE.PositionalAudio(listener); //
+		audioSource.setBuffer(audioclip);
+		camera.add(listener);
+		audioSource.setRefDistance(100);
+
 		//models
 
 		const bear = await loadGLTF("./assets/models/musicband-bear/scene.gltf");
 		bear.scene.scale.set(0.1, 0.1, 0.1);
-		const bearAnchor = mindarThree.addAnchor(0);
-		bearAnchor.group.add(bear.scene);
+		//const bearAnchor = mindarThree.addAnchor(0);
+		//bearAnchor.group.add(bear.scene);
 
 		const raccoon = await loadGLTF("./assets/models/musicband-raccoon/scene.gltf");
 		raccoon.scene.scale.set(0.1, 0.1, 0.1);
 		const raccoonAnchor = mindarThree.addAnchor(1);
-		raccoonAnchor.group.add(raccoon.scene);
+		raccoonAnchor.group.add(raccoon.scene, audioSource);
+
+		const donut = await loadGLTF("./assets/models/d1.glb");
+		donut.scene.scale.set(10, 10, 10);
+		const donutAnchor = mindarThree.addAnchor(0);
+		donutAnchor.group.add(donut.scene, audioSource);
 
 		// gltf animations
 		const mixer = new THREE.AnimationMixer(raccoon.scene);
@@ -36,10 +52,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		raccoonAnchor.onTargetFound = () => {
 			action.play();
+			audioSource.play();
 			console.log("raccoon found");
 		};
 		raccoonAnchor.onTargetLost = () => {
 			action.stop();
+			audioSource.stop();
 			console.log("raccoon lost");
 		};
 
@@ -53,6 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			raccoon.scene.rotation.set(0, raccoon.scene.rotation.y + delta, 0);
 			renderer.render(scene, camera);
 		});
-	};;
+	};
+
 	start();
 });
